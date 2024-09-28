47e71cf7f349ab3fedbf6da3c8a65479f7904255
jeman618 <juanci8827_.com@icloud.com>
2024-09-27 21:26:06 -0700

    Update bottler.py

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Depends
 from enum import Enum
 from pydantic import BaseModel
 from src.api import auth
+import sqlalchemy
+from src import database as db
 
 router = APIRouter(
     prefix="/bottler",
@@ -16,12 +18,18 @@ class PotionInventory(BaseModel):
 @router.post("/deliver/{order_id}")
 def post_deliver_bottles(potions_delivered: list[PotionInventory], order_id: int):
     """ """
+    with db.engine.begin() as connection:
+        sql_update = "UPDATE global_inventory SET num_green_ml = num_green_ml - 1 WHERE num_green_ml < 10"
+        connection.execute(sqlalchemy.text(sql_update))  
     print(f"potions delievered: {potions_delivered} order_id: {order_id}")
 
     return "OK"
 
 @router.post("/plan")
 def get_bottle_plan():
+    with db.engine.begin() as connection:
+        sql_to_execute = "SELECT num_green_ml FROM global_inventory"
+        result = connection.execute(sqlalchemy.text(sql_to_execute))
     """
     Go from barrel to bottle.
     """
@@ -34,8 +42,8 @@ def get_bottle_plan():
 
     return [
             {
-                "potion_type": [100, 0, 0, 0],
-                "quantity": 5,
+                "potion_type": [0, 100, 0, 0],
+                "quantity": result,
             }
         ]
 
