df66466be086b9d888624a4e24b0edc57ac67bfb
jeman618 <juanci8827_.com@icloud.com>
2024-09-29 12:12:42 -0700

    Update bottler.py

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -42,7 +42,7 @@ def get_bottle_plan():
 
     return [
             {
-                "potion_type": [0, 100, 0, 0],
+                "potion_type": [0, 1, 0, 0],
                 "quantity": result,
             }
         ]
