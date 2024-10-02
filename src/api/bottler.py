f37f489479b8db91a5945efca688028a16fd18b4
jeman618 <juanci8827_.com@icloud.com>
2024-10-02 10:53:27 -0700

    Update bottler.py

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -19,7 +19,8 @@ class PotionInventory(BaseModel):
 def post_deliver_bottles(potions_delivered: list[PotionInventory], order_id: int):
     """ """
     with db.engine.begin() as connection:
-        sql_update = "UPDATE global_inventory SET num_green_ml = SUM(num_green_ml) - 100 WHERE num_green_ml > 0"
+        sql_update = """UPDATE global_inventory 
+                            SET num_green_potions = num_green_potions + 1"""
         connection.execute(sqlalchemy.text(sql_update))  
     print(f"potions delievered: {potions_delivered} order_id: {order_id}")
 
@@ -28,7 +29,7 @@ def post_deliver_bottles(potions_delivered: list[PotionInventory], order_id: int
 @router.post("/plan")
 def get_bottle_plan():
     with db.engine.begin() as connection:
-        sql_to_execute = "UPDATE global_inventory SET num_green_ml = SUM(num_green_ml) + 100"
+        sql_to_execute = "UPDATE global_inventory SET num_green_potions = num_green_potions + 1"
         result = connection.execute(sqlalchemy.text(sql_to_execute))
     """
     Go from barrel to bottle.
