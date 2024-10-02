289f06232ed9ed8a04973eb2e18ed19186e1e75c
jeman618 <juanci8827_.com@icloud.com>
2024-10-02 10:59:14 -0700

    Update catalog.py

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -18,7 +18,7 @@ def get_catalog():
             {
                 "sku": "GREEN_POTION_0",
                 "name": "green potion",
-                "quantity": int(result),
+                "quantity":str(result),
                 "price": 50,
                 "potion_type": [0, 100, 0, 0],
             }
