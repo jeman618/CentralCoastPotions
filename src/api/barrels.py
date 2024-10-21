b6f087f2433cce5b17c9e7656592bee049a40aae
jeman618 <jcisne23@calpoly.edu>
2024-10-21 13:35:33 -0700

    customization

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -29,8 +29,8 @@ def post_deliver_barrels(barrels_delivered: list[Barrel], order_id: int):
     total_price = 0
 
     for barrel_delivered in barrels_delivered:
-          
           total_price += (barrel_delivered.price * barrel_delivered.quantity)
+
           red += barrel_delivered.potion_type[0] * barrel_delivered.quantity * barrel_delivered.ml_per_barrel
           green += barrel_delivered.potion_type[1] * barrel_delivered.quantity * barrel_delivered.ml_per_barrel
           blue += barrel_delivered.potion_type[2] * barrel_delivered.quantity * barrel_delivered.ml_per_barrel
@@ -57,30 +57,44 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
 
     gold_SQL = "SELECT gold FROM global_inventory"
     types_SQL = "SELECT potion_type FROM catalog"
+    ml_SQL = "SELECT red_ml, green_ml, blue_ml, dark_ml FROM global_inventory"
     p_inventory_SQL = "SELECT SUM(inventory) FROM catalog"
 
     with db.engine.begin() as connection:
                 gold = connection.execute(sqlalchemy.text(gold_SQL)).scalar()
                 types = connection.execute(sqlalchemy.text(types_SQL)).all()
                 potion_inventory = connection.execute(sqlalchemy.text(p_inventory_SQL)).scalar()
+                ml = connection.execute(sqlalchemy.text(ml_SQL)).fetchone()
 
+    ml = list(ml)
     gold_amount = gold
     plan = []
     total_price = 0
 
     if(potion_inventory < 10):
-        
         for barrel in wholesale_catalog:
             if (gold_amount - (barrel.price * barrel.quantity) > 0):
                     print(barrel)
-                
                     total_price += (barrel.price * barrel.quantity)
                     gold_amount -= total_price
+                    if (min(ml) == ml[0] and barrel.potion_type == [1,0,0,0]):
+                        barrel.potion_type = [1,0,0,0]
+                        ml[0] += barrel.ml_per_barrel
+                    elif (min(ml) == ml[1] and barrel.potion_type == [0,1,0,0]):
+                        ml[1] += barrel.ml_per_barrel
+                    elif (min(ml) == ml[2] and barrel.potion_type == [0,0,1,0]):
+                        ml[2] += barrel.ml_per_barrel
+                    elif (min(ml) == ml[3] and barrel.potion_type == [0,0,0,1]):
+                        ml[3] += barrel.ml_per_barrel
+                    else:
+                         ml[0] += barrel.ml_per_barrel 
+                    
                     plan.append({"sku": barrel.sku,
                                 "ml_per_barrel": barrel.ml_per_barrel,
                                 "potion_type": barrel.potion_type,
                                 "quantity": barrel.quantity,
                                 "price": barrel.price})
+                    
                           
     print(f"Total gold paid: {total_price}")
     print(plan)
